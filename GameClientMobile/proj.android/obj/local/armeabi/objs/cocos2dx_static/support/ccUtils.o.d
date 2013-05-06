d283e14f51638aa102bbd7078f8376a42c0674f2
SungJinYoo <dosm123@gmail.com>
2013-05-07 00:35:27 +0900

    removed unused files

--- GameClientMobile/proj.android/obj/local/armeabi/objs/cocos2dx_static/support/ccUtils.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/support/ccUtils.o: \
- /Users/sungjinyoo/Documents/Game/GameClient/GameClientMobile/proj.android/../GameClientMobile/libs/cocos2dx/support/ccUtils.cpp \
- /Users/sungjinyoo/Documents/Game/GameClient/GameClientMobile/proj.android/../GameClientMobile/libs/cocos2dx/support/ccUtils.h
-
-/Users/sungjinyoo/Documents/Game/GameClient/GameClientMobile/proj.android/../GameClientMobile/libs/cocos2dx/support/ccUtils.h:
