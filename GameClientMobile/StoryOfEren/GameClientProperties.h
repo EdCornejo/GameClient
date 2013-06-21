5ea1e784e009a4998924f3bb07c4dc5edd8f7a89
SungJinYoo <dosm123@gmail.com>
2013-06-21 22:06:44 +0900

    server port changed app splash image setting changed

--- GameClientMobile/StoryOfEren/GameClientProperties.h
+++ GameClientMobile/StoryOfEren/GameClientProperties.h
@@ -12,12 +12,12 @@
 #define THREAD_SCHEDULING_TIMESLICE     30                  //milliseconds
 
 #define FESERVER_CF_CONNECT_ADDRESS     "61.43.139.149"
-#define FESERVER_CF_CONNECT_PORT        1990
+#define FESERVER_CF_CONNECT_PORT        2967
 
 //#define FESERVER_CF_CONNECT_ADDRESS "192.168.0.13"
 //#define SERVER_CONNECT_ADDRESS          "61.43.139.149"         //loop back for test server
 //#define SERVER_CONNECT_ADDRESS          "127.0.0.1"         //loop back for test server
-#define SERVER_CONNECT_PORT             1988
+#define SERVER_CONNECT_PORT             1788
 
 //#define OBSERVER_ENABLE // make playerID = 1 to observer character
 
